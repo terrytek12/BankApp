b9c36bdcae3cc9ae9f3d72155a9b1571f4819cba
terrytek12 <terrytek12@gmail.com>
2023-12-03 11:10:47 -0800

    Updated Junit testing conditions for TellerAccount

--- myTest/src/myTesting/TellerAccountJUnitTest.java
+++ myTest/src/myTesting/TellerAccountJUnitTest.java
@@ -7,48 +7,67 @@ import org.junit.jupiter.api.Test;
 class TellerAccountJUnitTest {
 
 	@Test
-	void testTellerAccount() {
-		fail("Not yet implemented");
+	public void testTellerAccount() {
+		TellerAccount tellerTest = new TellerAccount ("Jack", "Davis", "jackdavis12@gmail.com", "hellothere"); 
+		
+	}
+
+	
+	//getter Junit tests
+	@Test
+	public void testGetFirstName() {
+		TellerAccount tellerTest = new TellerAccount ("Jack", "Davis", "jackdavis12@gmail.com", "hellothere");
+		assertEquals("Jack", tellerTest.getFirstName());
+	}
+	
+	@Test
+	public void testGetLastName() {
+		TellerAccount tellerTest = new TellerAccount ("Jack", "Davis", "jackdavis12@gmail.com", "hellothere");
+		assertEquals("Davis", tellerTest.getLastName());
+	}
+	
+	@Test
+	public void testGetEmail() {
+		TellerAccount tellerTest = new TellerAccount ("Jack", "Davis", "jackdavis12@gmail.com", "hellothere");
+		assertEquals("jackdavis12@gmail.com", tellerTest.getEmail());
+	}
+	
+	@Test
+	public void testGetPassword() {
+		TellerAccount tellerTest = new TellerAccount ("Jack", "Davis", "jackdavis12@gmail.com", "hellothere");
+		assertEquals ("hellothere", tellerTest.getPassword());
+		
+	}
+	
+	
+	//setter Junit tests
+	@Test
+	public void testSetFirstName() {
+		TellerAccount tellerTest = new TellerAccount ("Jack", "Davis", "jackdavis12@gmail.com", "hellothere");
+		tellerTest.setFirstName("Tom");
+		assertEquals ("Tom", tellerTest.getFirstName());
 	}
 
 	@Test
-	void testGetFirstName() {
-		fail("Not yet implemented");
+	public void testSetLastName() {
+		TellerAccount tellerTest = new TellerAccount ("Jack", "Davis", "jackdavis12@gmail.com", "hellothere");
+		tellerTest.setLastName("Cruise");
+		assertEquals ("Cruise", tellerTest.getLastName());
+		
 	}
 
 	@Test
-	void testSetFirstName() {
-		fail("Not yet implemented");
+	public void testSetEmail() {
+		TellerAccount tellerTest = new TellerAccount ("Jack", "Davis", "jackdavis12@gmail.com", "hellothere");
+		tellerTest.setEmail("tomcruise19@aol.com");
+		assertEquals("tomcruise19@aol.com", tellerTest.getEmail());
 	}
 
 	@Test
-	void testGetLastName() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetLastName() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetEmail() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetEmail() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetPassword() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetPassword() {
-		fail("Not yet implemented");
+	public void testSetPassword() {
+		TellerAccount tellerTest = new TellerAccount ("Jack", "Davis", "jackdavis12@gmail.com", "hellothere");
+		tellerTest.setPassword("luckyducky12");
+		assertEquals("luckyducky12", tellerTest.getPassword());
 	}
 
 }
