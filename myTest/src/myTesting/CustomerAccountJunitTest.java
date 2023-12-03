943f7a8b2b70ba4d728a9c1fb94fbdf36c488879
Richard <rtenorio@horizon.csueastbay.edu>
2023-12-02 21:03:30 -0800

    Added Customer Account Junit tests: BankAccount and toString tests. All CustomerAccount Junit tests pass.

--- myTest/src/myTesting/CustomerAccountJunitTest.java
+++ myTest/src/myTesting/CustomerAccountJunitTest.java
@@ -3,25 +3,41 @@ package myTesting;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.Before;
 
 class CustomerAccountJunitTest {
-
+/*
+	CustomerAccount testCA;
+	BankAccount testBA;
+	@Before public void initialize() {
+	this.testCA = new CustomerAccount("jdoe@gmail.com","John","Doe","1234");
+	this.testCA.setStreetNumber(101);testCA.setStreetName("2nd St");testCA.setCity("Hayward");testCA.setState("California");
+	this.testCA.setZip(94544);testCA.setStreetAddress();
+	this.testBA = new BankAccount(1234, 0.0, "John", "Doe");
+	}
+	*/
 	@Test
-	void testCustomerAccountConstructor() {
+	public void testCustomerAccountConstructor() {
 		CustomerAccount testCA = new CustomerAccount("jdoe@gmail.com","John","Doe","0123");
 		
 		testCA.setStreetNumber(101);testCA.setStreetName("2nd St");testCA.setCity("Hayward");testCA.setState("California");
 		testCA.setZip(94544);testCA.setStreetAddress();
+		
 		
 		assertEquals("jdoe@gmail.com",testCA.getEmail());
 		assertEquals("John Doe",testCA.getFirstName()+" "+testCA.getLastName());
 		assertEquals("101 2nd St", testCA.getStreetAddress());
 	}
 
-
+	@Test
+	public void testCustomerAccountsetPass() {
+		CustomerAccount testCA = new CustomerAccount("jdoe@gmail.com","John","Doe","0123");
+		testCA.setPass("4321");
+		assertEquals("4321",testCA.getPassword());
+	}
 	
 	@Test
-	void testCustomerAccountAddBankAccount() {
+	public void testCustomerAccountBankAccount() {
 		CustomerAccount testCA = new CustomerAccount("jdoe@gmail.com","John","Doe","1234");
 		
 		testCA.setStreetNumber(101);testCA.setStreetName("2nd St");testCA.setCity("Hayward");testCA.setState("California");
@@ -30,11 +46,18 @@ class CustomerAccountJunitTest {
 		testCA.AddBankAccount(testBA);
 		
 		int testid = testBA.getId();
-		testCA.getBankAccount(testid);
-		System.out.println(testid);
-		System.out.println(testCA.getBankAccount(testid));
-		System.out.println(testBA.getPin());
+		
+		assertEquals(testCA.getBAbyIndex(0),testCA.getBankAccount(testid));
 	
+	}
+	
+	@Test
+	public void testCustomerAccounttoString() {
+		CustomerAccount testCA = new CustomerAccount("jdoe@gmail.com","John","Doe","1234");
+		
+		String testString = testCA.toString();
+		
+		assertEquals("jdoe@gmail.com,John,Doe,1234,\n",testString);
 	}
 	
 }
